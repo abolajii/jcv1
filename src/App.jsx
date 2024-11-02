7cafc7a2f83a15644aea99a36d22d7d55135cbf0
abolajii <abolajiadeajayi@gmail.com>
2024-11-02 11:01:21 +0100

    mobile

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,10 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: space-between;
+  // Hide on desktop
+  @media (min-width: 769px) {
+    display: none;
+  }
 `;
 
 const Scrollable = styled.div`
@@ -37,6 +41,11 @@ const MobileSidebar = styled.div`
   height: 70px;
   background-color: #f2f2f2;
   box-shadow: 0 -2px 5px rgba(0, 0, 0, 0.3);
+
+  // Hide on desktop
+  @media (min-width: 769px) {
+    display: none;
+  }
 `;
 
 const Header = styled.div`
@@ -48,6 +57,18 @@ const Header = styled.div`
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
 `;
 
+const DeskTop = styled.div`
+  display: none; /* Hidden by default */
+
+  // Show only on desktop
+  @media (min-width: 769px) {
+    display: block;
+    background-color: green;
+    color: white;
+    padding: 20px;
+  }
+`;
+
 const App = () => {
   return (
     <>
@@ -57,64 +78,13 @@ const App = () => {
         <Scrollable>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit sit
           eligendi, iure quaerat, voluptates magnam excepturi natus quod, sed
-          saepe consequuntur neque deserunt explicabo ullam reprehenderit. Modi
-          illo, minima earum delectus nostrum provident laudantium consequuntur
-          architecto deserunt aliquid laboriosam. Eum quia veritatis nulla in.
-          Eaque asperiores quo deserunt corporis quaerat. Deserunt tempora a
-          tempore adipisci et. Explicabo, ullam. Ea, nisi sed voluptatibus,
-          voluptatum blanditiis alias distinctio ab ut eum assumenda consequatur
-          et? Doloribus ullam explicabo repellat praesentium sapiente quasi
-          delectus corrupti! Aut similique modi voluptatem animi placeat
-          molestiae ea aperiam, corporis accusantium quasi harum dolorum nam
-          sint, porro nemo corrupti? Labore dolore distinctio harum odit ratione
-          fugit amet voluptas magni vitae, voluptates veritatis voluptatem sit
-          saepe itaque asperiores optio repellendus, porro animi quia debitis
-          rerum enim impedit vel. Consectetur, quod. Debitis vel officiis,
-          itaque suscipit atque, necessitatibus consequuntur aperiam nobis nam
-          eligendi facere? Ad qui distinctio dolorem voluptate tempore
-          perferendis illum quos quas voluptatibus blanditiis commodi soluta,
-          fugit voluptatem maiores. Sequi error repellat quidem commodi
-          recusandae soluta aliquid explicabo corrupti omnis beatae, hic
-          accusantium nemo qui sint fugiat quo amet tempora voluptatum
-          similique. Iste veniam natus consequuntur labore. Tenetur neque eos,
-          quidem recusandae fugiat blanditiis illo nostrum unde repudiandae quis
-          temporibus eum ipsum perspiciatis tempora qui laborum esse odit
-          similique. Suscipit pariatur veniam eaque obcaecati cupiditate alias
-          sint nisi adipisci, illo consequuntur maiores, in autem asperiores
-          dolore iusto fuga itaque voluptas amet ipsam sit. Doloremque, quae ut
-          qui repellendus dignissimos quaerat ratione ducimus molestiae omnis
-          nobis libero modi, asperiores eveniet praesentium dolor. Voluptatum
-          dolore id earum laboriosam magni esse aliquam corrupti, sit ea
-          adipisci! Fugiat, sint itaque! Necessitatibus expedita assumenda
-          ipsum, aliquam quae fuga sequi architecto quia nostrum officia eveniet
-          ea rerum, omnis accusantium quas doloremque at tenetur reiciendis
-          harum porro labore cupiditate beatae? Incidunt accusamus dolorem
-          adipisci. Architecto eius, maxime optio dolorum officia ipsum facere
-          odit nostrum sit quaerat at mollitia deserunt iste incidunt impedit
-          ipsam fugiat amet aut eaque alias reiciendis possimus sed! Eaque nulla
-          vel similique dolores necessitatibus in consequatur velit ex
-          repudiandae distinctio repellat perferendis aut, officia pariatur ab,
-          molestiae cum doloribus enim quibusdam iusto obcaecati omnis odio a?
-          Eaque, quasi. Corporis dolores laudantium omnis quidem aliquid? Quidem
-          consequatur reprehenderit libero accusamus, blanditiis porro suscipit
-          dolore reiciendis maxime quo sequi assumenda expedita! Debitis aperiam
-          fugiat voluptatibus nihil facere exercitationem ipsam! Eum nemo cum
-          vel distinctio soluta sapiente sint, nisi rerum omnis magnam optio
-          sunt quas, accusantium quis modi provident vero voluptatem
-          consequatur. Corrupti, fuga aut sed dolorem laudantium, earum expedita
-          quo nostrum autem molestiae facilis eveniet, hic aperiam incidunt. Et
-          incidunt vitae recusandae, culpa ullam excepturi quis optio eligendi
-          saepe impedit cupiditate ad dolores debitis enim assumenda sed
-          repellat eos. Quod qui sint voluptates, atque libero repellendus eius
-          fugit aut eum rerum perspiciatis delectus quo, ipsam dicta, quisquam
-          inventore ratione voluptatum corporis. Quisquam sunt commodi quos
-          architecto similique, eligendi eum quo veniam dolorem, eveniet aliquam
-          nostrum vero blanditiis labore illum voluptatem officiis ullam eos
-          quia doloribus! Suscipit reiciendis labore cupiditate, quam dolorum
-          eos iste voluptatum voluptates?
+          saepe consequuntur neque deserunt explicabo ullam reprehenderit. ...
         </Scrollable>
-        <MobileSidebar />
+        <MobileSidebar>Mobile Sidebar</MobileSidebar>
       </Container>
+      <DeskTop>
+        <div>Desktop Content</div>
+      </DeskTop>
     </>
   );
 };
